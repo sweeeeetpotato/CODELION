e533a23831f04dce53bae939c2de1e2c0e046ea9
sweeeeetpotato <sweet_potato13@naver.com>
2022-04-27 21:17:39 +0900

    22일차 수업내용 업로드

---
+++ 수업내용/Day22/JavaScript/002.js
@@ -0,0 +1,5 @@
+document.getElementById("one").innerHTML = "hello <strong>world</strong> document";
+console.log("hello world console");
+console.error("hello world console");
+document.getElementById("two").innerText = "hello <strong>world</strong> document";
+document.write("hello!!");
